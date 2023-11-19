39e544b9230b8c8a728865d81294ec2de4a0a069
Your Name <samuelhafsteins@gmail.com>
2023-11-19 21:08:22 +0000

    remove unused pub

--- src/commands/malid.rs
+++ src/commands/malid.rs
@@ -35,7 +35,7 @@ fn format_specific_day(menu: &DailyMenu) -> Result<String, Error> {
   ))
 }
 
-pub async fn this_weeks_menu() -> Result<Vec<DailyMenu>, reqwest::Error> {
+async fn this_weeks_menu() -> Result<Vec<DailyMenu>, reqwest::Error> {
   let url = "https://prod-198.westeurope.logic.azure.com/workflows/cc7c4c7157b14d5ba688859712303172/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=cRM1huMwILXk-jf6xybnCcTRpnSxjKY53jFwwUGLx14";
   let menu: Vec<DailyMenu> = reqwest::get(url).await?.json::<Vec<DailyMenu>>().await?;
 
